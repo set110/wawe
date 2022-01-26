be51f65b5ae2fa84f8361ac20c1d39748970b4be
set110 <set110@ya.ru>
2022-01-26 18:29:42 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 $(function () {
+    $(".menu a, .footer__bottom a, .logo").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
     $('.slider__items').slick({
         dots:true,
         arrows:false,
        
      });
 
-     $('.menu__btn').on('click', function () {
+     $('.menu__btn, .menu a').on('click', function () {
         $('.header__top').toggleClass('header__top--active');
        
      });
