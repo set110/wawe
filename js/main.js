a0df832109e26efc872f5ab8688ab7628f1cde13
set110 <set110@ya.ru>
2022-01-26 14:56:21 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(function () {
-
     $('.slider__items').slick({
         dots:true,
         arrows:false,
+       
+     });
+
+     $('.menu__btn').on('click', function () {
+        $('.header__top').toggleClass('header__top--active');
        
      });
      
